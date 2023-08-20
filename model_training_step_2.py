22e4efe7d9e3b0c5619461a1d0b1d5c8955906a6
Raktim Mondol <raktim.live@gmail.com>
2023-08-20 16:23:07 +1000

    Update model_training_step_2.py

--- model_training_step_2.py
+++ model_training_step_2.py
@@ -1,40 +1,5 @@
 ### IMPORT LIBRARIES ###
-import os
-import glob
-import numpy as np
-import pandas as pd
-from PIL import Image
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-import random
-import csv
-import pandas as pd
-
-import matplotlib.pyplot as plt
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Dataset
-
-from torch.utils.tensorboard import SummaryWriter
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-from torch.utils.data import random_split
-
-from sklearn.manifold import TSNE
-from sklearn.preprocessing import StandardScaler
-from color_normalizer import MacenkoColorNormalization
-from data_load import PatientDataset 
-from torchvision.models import (resnet50, ResNet50_Weights, resnet18, ResNet18_Weights, 
-                                vit_l_32, ViT_L_32_Weights, vit_l_16, ViT_L_16_Weights, 
-                                maxvit_t, MaxVit_T_Weights, swin_b, Swin_B_Weights,
-                                efficientnet_v2_m, EfficientNet_V2_M_Weights)
-from scipy.stats import spearmanr
-import numpy as np
-from tqdm import tqdm
-import csv
+from libs import *
 
 def set_seeds(seed=0):
     random.seed(seed)
